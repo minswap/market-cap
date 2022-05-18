13b21dcc87e8e548ffdaf5c472a7f42deefb2724
Bhagavath Singh <rbhagavathsingh@gmail.com>
2022-05-18 15:06:45 -0400

    Fixed Formating

--- src/tokens/emp.ts
+++ src/tokens/emp.ts
@@ -2,17 +2,17 @@ import { defaultFetcherOptions, SupplyFetcher } from "../types";
 import { getAxiosInstance } from "../utils";
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
-    const axios = getAxiosInstance(options);
-    const total = 200_000_000; 
-    const circulating: number = await axios.get(
-        'https://token.empowa.io/emp/supply'
-    ).then(res => { res.data.circulating_amount / 1000000});
-    return {
-        circulating: circulating.toString(),
-        total: total.toString(),
-    };
+  const axios = getAxiosInstance(options);
+  const total = 200_000_000;
+  const circulating: number = await axios
+    .get("https://token.empowa.io/emp/supply")
+    .then((res) => {
+      res.data.circulating_amount / 1000000;
+    });
+  return {
+    circulating: circulating.toString(),
+    total: total.toString(),
+  };
 };
 
-
 export default fetcher;
-
