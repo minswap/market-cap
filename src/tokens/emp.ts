44a8b2b99a41cbcd45c9b4200a0049fd303b88b0
Long Nguyen <vulongvn98@gmail.com>
2022-05-19 17:26:48 +0700

    Added Circulating Supply for EMP (#7) (#8)
    * Added Circulating Supply for EMP (#7)
    
    * Added Circulating Supply for EMP
    
    Added Circulating Supply for EMP
    
    * Fixed Formating
    
    * fix conflict with main
    
    Signed-off-by: Nguyen Le Vu Long <vulongvn98@gmail.com>
    
    * fix EMP circulating supply
    
    Signed-off-by: Nguyen Le Vu Long <vulongvn98@gmail.com>
    
    Co-authored-by: Bhagavathsingh Raamsingh <32787303+rbhagavathsingh@users.noreply.github.com>

--- src/tokens/emp.ts
+++ src/tokens/emp.ts
@@ -1,8 +1,14 @@
-import { SupplyFetcher } from "../types";
+import { defaultFetcherOptions, SupplyFetcher } from "../types";
+import { getAxiosInstance } from "../utils";
 
-const fetcher: SupplyFetcher = async () => {
+const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
+  const axios = getAxiosInstance(options);
   const total = 200_000_000;
+  const circulating: number = await axios
+    .get("https://token.empowa.io/emp/supply")
+    .then((res) => res.data.circulating_amount / 1e6);
   return {
+    circulating: circulating.toString(),
     total: total.toString(),
   };
 };
