18e086ab676484902990882071c43dcca317359f
Nick Jorens <nickjorens@gmail.com>
2022-05-23 16:04:11 +0200

    Ran Prettier

--- src/tokens/yummi.ts
+++ src/tokens/yummi.ts
@@ -1,20 +1,28 @@
-import {defaultFetcherOptions, SupplyFetcher} from "../types";
-import {getAmountInAddresses, getBlockFrostInstance} from "../utils";
+import { defaultFetcherOptions, SupplyFetcher } from "../types";
+import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
 
-const YUMMI = "078eafce5cd7edafdf63900edef2c1ea759e77f30ca81d6bbdeec92479756d6d69";
-const YUMMI_POLICY_ID = "078eafce5cd7edafdf63900edef2c1ea759e77f30ca81d6bbdeec924";
-const STAKING_ADDRESS = "addr1wydpsqf5zz9ddy76d3f3jrrf6jkpyjr48nx5a706w9y68ucy4wu6s";
+const YUMMI =
+  "078eafce5cd7edafdf63900edef2c1ea759e77f30ca81d6bbdeec92479756d6d69";
+const YUMMI_POLICY_ID =
+  "078eafce5cd7edafdf63900edef2c1ea759e77f30ca81d6bbdeec924";
+const STAKING_ADDRESS =
+  "addr1wydpsqf5zz9ddy76d3f3jrrf6jkpyjr48nx5a706w9y68ucy4wu6s";
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
-    const blockFrost = getBlockFrostInstance(options);
-    const circulating = Number(await blockFrost.assetsPolicyById(YUMMI_POLICY_ID).then((resp) => resp[0].quantity));
-    const total = 1e10;
-    const staking =
-        Number(await getAmountInAddresses(blockFrost, YUMMI, [STAKING_ADDRESS]));
-    return {
-        circulating: (circulating - staking).toString(),
-        total: total.toString(),
-    };
+  const blockFrost = getBlockFrostInstance(options);
+  const circulating = Number(
+    await blockFrost
+      .assetsPolicyById(YUMMI_POLICY_ID)
+      .then((resp) => resp[0].quantity)
+  );
+  const total = 1e10;
+  const staking = Number(
+    await getAmountInAddresses(blockFrost, YUMMI, [STAKING_ADDRESS])
+  );
+  return {
+    circulating: (circulating - staking).toString(),
+    total: total.toString(),
+  };
 };
 
 export default fetcher;
