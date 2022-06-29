fa5ec0d13b200e0a7367b093e39ff4be3f520d4d
Patrick Tobler <patrick@tobler.de>
2022-06-29 05:13:53 +0200

    Add nmkr.ts (#17)
    Returns current circulating supply from Coingecko

---
+++ src/tokens/nmkr.ts
@@ -0,0 +1,16 @@
+import { defaultFetcherOptions, SupplyFetcher } from "../types";
+import { getAxiosInstance } from "../utils";
+
+const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
+  const axios = getAxiosInstance(options);
+  const total = 10_000_000_000;
+  const circulating: number = await axios(
+    "https://api.coingecko.com/api/v3/coins/nft-maker"
+  ).then((res) => res.data.market_data.circulating_supply);
+  return {
+    circulating: circulating.toString(),
+    total: total.toString(),
+  };
+};
+
+export default fetcher;
