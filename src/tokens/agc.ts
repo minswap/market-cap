7ec2d404ffcf5469d4798f34e159829d34a7c93e
Kurt Hartmann <extramileit@gmail.com>
2022-07-24 10:22:03 -0500

    modify total format

--- src/tokens/agc.ts
+++ src/tokens/agc.ts
@@ -1,7 +1,7 @@
 import { SupplyFetcher } from "../types";
 
 const fetcher: SupplyFetcher = async () => {
-    const total = 3e8;
+    const total = 300_000_000;
     return {
         total: total.toString(),
     };
