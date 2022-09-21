cfac80e88a97cfda85d32f306a5f1b4dfc97ef66
DEADPXLZ <83417674+DEADPXLZ@users.noreply.github.com>
2022-09-21 14:40:56 +0300

    fix formatting

--- src/tokens/ding.ts
+++ src/tokens/ding.ts
@@ -2,15 +2,15 @@ import { defaultFetcherOptions, SupplyFetcher } from "../types";
 import { getAxiosInstance } from "../utils";
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
-    const axios = getAxiosInstance(options);
-    const total = 1_000_000_000;
-    const circulating: number = await axios(
-        "https://universe.pxlz.org/circulating"
-    ).then((res) => res.data);
-    return {
-        circulating: circulating.toString(),
-        total: total.toString(),
-    };
+  const axios = getAxiosInstance(options);
+  const total = 1_000_000_000;
+  const circulating: number = await axios(
+    "https://universe.pxlz.org/circulating"
+  ).then((res) => res.data);
+  return {
+    circulating: circulating.toString(),
+    total: total.toString(),
+  };
 };
 
 export default fetcher;
