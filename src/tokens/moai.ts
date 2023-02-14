6426fe99a4e132a8059c0bdd52ff6558042a8ca0
Seanage <own_age@msn.com>
2023-02-14 17:18:02 -0600

    fixing formatting concern

--- src/tokens/moai.ts
+++ src/tokens/moai.ts
@@ -9,10 +9,9 @@ const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const treasuryRaw = await getAmountInAddresses(blockFrost, MOAI, [
     "stake1uyszp885h7z8fry5thjdcj5msk59ajsannca7x3g3d2ycvgqfq6t9", // Treasury
     "stake1u9afgy0npvt9sal5vzlm5t6n5uyvch8t9mmznhtkn9wjacgfdtldf", // Mutant Stake Rewards Distribution
-    "stake1ux07wmwzwv06kl02ct3m4lly569lsvjvp6za9756r8hdsmslyw5mv" // burn wallet
+    "stake1ux07wmwzwv06kl02ct3m4lly569lsvjvp6za9756r8hdsmslyw5mv", // burn wallet
   ]);
-  const treasury = Number(treasuryRaw); 
-  console.log(`Seantest: ${(total - treasury).toString()}`);
+  const treasury = Number(treasuryRaw);
   return {
     circulating: (total - treasury).toString(),
     total: total.toString(),
