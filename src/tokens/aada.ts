cd25328dd0c2c5adefa5bccdceb2433ef3280ecd
Aada Finance <89693711+aada-finance@users.noreply.github.com>
2023-03-12 17:35:42 +0200

    Adding aada treasury wallet
    Adding new Aada treasury wallet

--- src/tokens/aada.ts
+++ src/tokens/aada.ts
@@ -9,6 +9,7 @@ const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const treasuryRaw = await getAmountInAddresses(blockFrost, AADA, [
     "stake1u98ak0va7tgd23rl62tphdare6z602e0vx5m0fw4gdr65kq875eec",
     "stake1uyt8x8wwvn5ze8aw6eqs35w2znyh4xnn9xsu3jxjk503nxcmdkjuz",
+    "stake1u9hqmm7neuayxpm9962kk09x27yu5kmcx6h9f98tc4r2mzsyaeh40",
   ]);
   const treasury = Number(treasuryRaw) / 1e6;
   return {
