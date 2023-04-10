945ab06e8d3ae70e379bc9448c07a650768e0857
Cody Butz <codyjbutz@gmail.com>
2023-04-10 12:05:58 -0400

    Cleanup

--- src/tokens/indy.ts
+++ src/tokens/indy.ts
@@ -1,15 +1,14 @@
 import { defaultFetcherOptions, SupplyFetcher } from "../types";
-import { getAmountInAddresses, getAxiosInstance, getBlockFrostInstance } from "../utils";
+import { getAxiosInstance } from "../utils";
 
 const INDY = "533bb94a8850ee3ccbe483106489399112b74c905342cb1792a797a0494e4459";
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
-  const blockFrost = getBlockFrostInstance(options);
   const total = 35_000_000;
   const axios = getAxiosInstance(options);
   const response = await axios.get('https://analytics.indigoprotocol.io/api/stats/indy-circulating-supply');
   const treasury = Number(response.data.circulatingSupply) / 1e6;
-  
+
   return {
     circulating: treasury.toString(),
     total: total.toString(),
