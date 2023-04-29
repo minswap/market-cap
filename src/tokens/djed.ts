0a2c3384281353102478236438415e2b5cadb752
jacobwarren44 <jacob@taptools.io>
2023-04-29 17:38:42 -0400

    Run npm run format

--- src/tokens/djed.ts
+++ src/tokens/djed.ts
@@ -1,7 +1,8 @@
 import { defaultFetcherOptions, SupplyFetcher } from "../types";
 import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
 
-const DJED = "8db269c3ec630e06ae29f74bc39edd1f87c819f1056206e879a1cd61446a65644d6963726f555344";
+const DJED =
+  "8db269c3ec630e06ae29f74bc39edd1f87c819f1056206e879a1cd61446a65644d6963726f555344";
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const blockFrost = getBlockFrostInstance(options);
