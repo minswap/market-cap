7ef5d655f28b448bcbc1ec567e79e95cb2e3c72d
jacobwarren44 <jacob@taptools.io>
2023-04-30 11:32:42 -0400

    Update HOSKY circulating supply

--- src/tokens/hosky.ts
+++ src/tokens/hosky.ts
@@ -1,8 +1,23 @@
-import { SupplyFetcher } from "../types";
+import { defaultFetcherOptions, SupplyFetcher } from "../types";
+import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
 
-const fetcher: SupplyFetcher = async () => {
+const HOSKY =
+  "a0028f350aaabe0545fdcb56b039bfb08e4bb4d8c4d7c3c7d481c235484f534b59";
+
+const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
+  const blockFrost = getBlockFrostInstance(options);
   const total = 1e15;
+  const treasuryRaw = await getAmountInAddresses(blockFrost, HOSKY, [
+    "stake1uyhr6j0v95y3nwemsfyw8l3ant90wruclgwe9ktnm5ptk8qhfnyt6", // charles
+    "stake1uxgekrcht2nz7eag9kffkh5s2hcpqswaakf9synqdhs82zg6x25pc", // tbd
+    "stake1u87awjfd5mdufylf0zzeuwz6vpllp0wz0elklphl833mqrchvwzys", // rugpool
+    "stake1ux7fgdkwjwgyer0ldfdh89fmq0n2awjzsy0jzkuyp4wxcmqn5239v", // founder 1
+    "stake1u87gsquxhu2lp59n09tr3pmj0yqll5s3kearzpvlnxgj5xq35pufw", // founder 2
+    "stake1u83p8p9uvdcglucefnxxjc79u54zv3e3qptsx5qjcwxq5rcjzrlwk", // meme
+  ]);
+  const treasury = Number(treasuryRaw);
   return {
+    circulating: (total - treasury).toString(),
     total: total.toString(),
   };
 };
