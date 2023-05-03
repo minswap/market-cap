c86823f230c41af03cb8b8c62d248890fa10d16b
jacobwarren44 <jacob@taptools.io>
2023-05-03 09:58:52 -0400

    Get DJED/SHEN smart contracts by DjedStableCoinNFT

--- src/tokens/djed.ts
+++ src/tokens/djed.ts
@@ -3,12 +3,15 @@ import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
 
 const DJED =
   "8db269c3ec630e06ae29f74bc39edd1f87c819f1056206e879a1cd61446a65644d6963726f555344";
+const DJEDNFT =
+  "8db269c3ec630e06ae29f74bc39edd1f87c819f1056206e879a1cd61446a6564537461626c65436f696e4e4654";
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const blockFrost = getBlockFrostInstance(options);
   const total = 1e12;
+  const addresses = await blockFrost.assetsAddresses(DJEDNFT);
   const treasuryRaw = await getAmountInAddresses(blockFrost, DJED, [
-    "stake1uyd6tfxa3sae586zjvll7qjx8ywj9x8l3dddgc8dkc0tshssd5g6e",
+    addresses[0]["address"],
   ]);
   const treasury = Number(treasuryRaw) / 1e6;
   return {
