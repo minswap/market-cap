274eb2eec9d01b4713ca48749a2a82138a2b296d
similiken <122702393+similiken@users.noreply.github.com>
2023-05-07 20:26:23 -0400

    Add ashib

---
+++ src/tokens/ashib.ts
@@ -0,0 +1,9 @@
+import { defaultFetcherOptions, SupplyFetcher } from "../types";
+
+const ASHIB = "afc910d7a306d20c12903979d4935ae4307241d03245743548e767834153484942";
+
+const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
+    const total = 109000000000;
+};
+    
+export default fetcher;
