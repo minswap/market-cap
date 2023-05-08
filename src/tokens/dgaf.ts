dbaac260173b6bb70a75df57886849eadfbd2a35
Legionaaa <jcabradley@gmail.com>
2023-05-08 13:46:20 +0000

    Fixes

--- src/tokens/dgaf.ts
+++ src/tokens/dgaf.ts
@@ -1,18 +1,17 @@
 import { defaultFetcherOptions, SupplyFetcher } from "../types";
 import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
 
-const DGAF =
-  "64c3ebd40ed377989aa3069a2936e07c6ce82df46688c473d921520664676166";
+const DGAF = "64c3ebd40ed377989aa3069a2936e07c6ce82df46688c473d921520664676166";
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const blockFrost = getBlockFrostInstance(options);
   const total = 100_000_000_000;
-  const treasury = await getAmountInAddresses(blockFrost, DGAF, [
-    "stake1u9k7mcf6zvun9wlfrkwsg2w8sy95302gpnj73ly0l6tqhks997y0t" // Treasury
+  const treasuryRaw = await getAmountInAddresses(blockFrost, DGAF, [
+    "stake1u9k7mcf6zvun9wlfrkwsg2w8sy95302gpnj73ly0l6tqhks997y0t", // dgaf treasury + vault allocation
   ]);
-
+  const treasury = Number(treasuryRaw);
   return {
-    circulating: (total - Number(treasury) / 1e6).toString(),
+    circulating: (total - treasury).toString(),
     total: total.toString(),
   };
 };
