0a0d26096b0e8869555cfdd7087f27a0c2158572
Ryan Felt <ryanfelt@ryans-mbp.lan>
2023-05-09 11:25:49 -0400

    feat: VYFI token circulating supply

--- src/tokens/vyfi.ts
+++ src/tokens/vyfi.ts
@@ -1,8 +1,18 @@
-import { SupplyFetcher } from "../types";
+import { defaultFetcherOptions, SupplyFetcher } from "../types";
+import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
 
-const fetcher: SupplyFetcher = async () => {
+const VYFI =
+  "804f5544c1962a40546827cab750a88404dc7108c0f588b72964754f";
+
+const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
+  const blockFrost = getBlockFrostInstance(options);
   const total = 450_000_000;
+  const treasuryRaw = await getAmountInAddresses(blockFrost, VYFI, [
+    "addr1qx6jq2ns6g9t4y8zyf2ahmhjzw8k6h3a7fy5tkqwgnwgh09z80nptm8at52jqjauttsswp6d7h9rq807z80mqhge5aqqctjalg",
+  ]);
+  const treasury = Number(treasuryRaw) / 1e6;
   return {
+    circulating: (total - treasury).toString(),
     total: total.toString(),
   };
 };
