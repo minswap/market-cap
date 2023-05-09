e0e90e76c730174f4c669bc334dc502b5dcf14be
4TT1L4 <2914096+4TT1L4@users.noreply.github.com>
2023-05-09 09:36:28 +0200

    Added GENS to the index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ import dgafFetcher from "./tokens/dgaf";
 import dingFetcher from "./tokens/ding";
 import discoFetcher from "./tokens/disco";
 import empFetcher from "./tokens/emp";
+import gensFetcher from "./tokens/gens";
 import geroFetcher from "./tokens/gero";
 import herbFetcher from "./tokens/herb";
 import hoskyFetcher from "./tokens/hosky";
@@ -72,6 +73,8 @@ export const supplyFetchers: Record<string, SupplyFetcher> = {
     vyfiFetcher,
   a0028f350aaabe0545fdcb56b039bfb08e4bb4d8c4d7c3c7d481c235484f534b59:
     hoskyFetcher,
+  dda5fdb1002f7389b33e036b6afee82a8189becb6cba852e8b79b4fb0014df1047454e53:
+    gensFetcher,
   "10a49b996e2402269af553a8a96fb8eb90d79e9eca79e2b4223057b64745524f":
     geroFetcher,
   "8e51398904a5d3fc129fbf4f1589701de23c7824d5c90fdb9490e15a434841524c4933":
