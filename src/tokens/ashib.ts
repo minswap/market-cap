d10769adef56309b3e56e4c4d684ac6c007b7a72
similiken <122702393+similiken@users.noreply.github.com>
2023-05-14 13:54:19 -0400

    Add ashib - now with return of function
    This file has return of function included compared to the previous pull request.

---
+++ src/tokens/ashib.ts
@@ -0,0 +1,12 @@
+import { defaultFetcherOptions, SupplyFetcher } from "../types";
+
+const ASHIB = "afc910d7a306d20c12903979d4935ae4307241d03245743548e767834153484942";
+
+const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
+    const total = 109000000000;
+    return {
+        total: total.tostring(),
+        };
+};
+    
+export default fetcher;
