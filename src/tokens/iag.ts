bdc1911b1ee60e5e7361e53a54ebd7d3f5b0681d
shrijana <shrijana1991@gmail.com>
2023-05-24 11:27:11 +0545

    update comment

--- src/tokens/iag.ts
+++ src/tokens/iag.ts
@@ -5,8 +5,8 @@ const IAG = "5d16cc1a177b5d9ba9cfa9793b07e60f1fb70fea1f8aef064415d114494147";
 const TREASURY_ADDRESSES = [
   "stake1u9cms5rnmqjed2dxfs9xcq5zupxpcq3l0jq492te2auhppcnlfdsv", // Ecosystem Development Fund
   "stake1uyc5ycj8e8fq0ss6fpjv7627tczcwn26w0lrql2gcflqm0q2xd35e", // Community Incentives
-  "stake1uxc38mnkfvsm0yc2un6g8yp49nshnv2t4rcvqapeahnm87ccz9ns8", // Liquidity
-  "stake1uy8a6tmpcy9mh57m8puka0ra0zq2zt4pjcuphyh770u3spg8cn9zk", // Liquidity
+  "stake1uxc38mnkfvsm0yc2un6g8yp49nshnv2t4rcvqapeahnm87ccz9ns8", // Liquidity 1
+  "stake1uy8a6tmpcy9mh57m8puka0ra0zq2zt4pjcuphyh770u3spg8cn9zk", // Liquidity 2
   "stake1u9gc67yuparwf2qjvvsv5c652yf8zfcn7wwtmruap0fte6qyz6h9g", // Development
   "stake1ux0x89ur69hu2gqvsaw8fpt45zj4kt50h8lq4ycd68ycsmc9m4y99", // Advisors / Marketing
   "stake1uywysqcqqys7qewrcl0uhda8g25ecr5k2klsr2d45jmdpnsqmc5hh", // Team
