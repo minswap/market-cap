31a13830d8cac124f91ce7ed8339aa47efe0d5da
shrijana <shrijana1991@gmail.com>
2023-05-24 10:51:53 +0545

    Add IAG circualting supply

--- src/tokens/iag.ts
+++ src/tokens/iag.ts
@@ -1,8 +1,25 @@
-import { SupplyFetcher } from "../types";
+import { defaultFetcherOptions, SupplyFetcher } from "../types";
+import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
 
-const fetcher: SupplyFetcher = async () => {
-  const total = 1e9;
+const IAG = "5d16cc1a177b5d9ba9cfa9793b07e60f1fb70fea1f8aef064415d114494147";
+const TREASURY_ADDRESSES = [
+  "stake1u9cms5rnmqjed2dxfs9xcq5zupxpcq3l0jq492te2auhppcnlfdsv", // Ecosystem Development Fund
+  "stake1uyc5ycj8e8fq0ss6fpjv7627tczcwn26w0lrql2gcflqm0q2xd35e", // Community Incentives
+  "stake1uxc38mnkfvsm0yc2un6g8yp49nshnv2t4rcvqapeahnm87ccz9ns8", // Liquidity
+  "stake1uy8a6tmpcy9mh57m8puka0ra0zq2zt4pjcuphyh770u3spg8cn9zk", // Liquidity
+  "stake1u9gc67yuparwf2qjvvsv5c652yf8zfcn7wwtmruap0fte6qyz6h9g", // Development
+  "stake1ux0x89ur69hu2gqvsaw8fpt45zj4kt50h8lq4ycd68ycsmc9m4y99", // Advisors / Marketing
+  "stake1uywysqcqqys7qewrcl0uhda8g25ecr5k2klsr2d45jmdpnsqmc5hh"  // Team
+];
+
+const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
+  const blockFrost = getBlockFrostInstance(options);
+  const total = 1e9; // 1 billion
+  const treasury =
+    Number(await getAmountInAddresses(blockFrost, IAG, TREASURY_ADDRESSES)) /
+    1e6;
   return {
+    circulating: (total - treasury).toString(),
     total: total.toString(),
   };
 };
