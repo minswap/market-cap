95f703d3fe1208d5de579bbe6e331ebc5265441f
jacobwarren44 <jacob@taptools.io>
2023-07-09 09:13:57 -0400

    Fix LQ, PAVIA, VYFI

--- src/tokens/lq.ts
+++ src/tokens/lq.ts
@@ -7,10 +7,10 @@ const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const blockFrost = getBlockFrostInstance(options);
   const total = 21_000_000;
   const treasuryRaw = await getAmountInAddresses(blockFrost, LQ, [
-    "stake17xuctqah3vtn232230mgnh8j0g0c9xe9j59a5jv54v2m6lghq7wjg", // user dist
-    "stake17y92lfz032ahf6xly5fp3l8k28xzpzullx46ffg8vmt7xpquzjyrp", // team
-    "stake179h0rfqsl5qhl7mh9wrazeel0wkzxck8zaq6et4k3jtz96c374qun", // treasury
-    "stake1782eemwxrrfmzga0yq5eewhu5cqe6p0n0wa8xwzhwkwhl4q327q0r", // staking
+    "stake17yl23x3q5v4hpjyywjal326ku2953kn4dhkrq5edmqcyj4cz0h7v5", // user dist
+    "stake17x5s640797a4wlu0x3wtxrytrq8hx67smk238vpvjgjkjgqhmc4sl", // team
+    "stake17yr6hxevthv7fr6la584r3sy4jlgf7uwtc2udkwzj8zt3vg2mzq4x", // treasury
+    "stake17xljj9au6hels8vtn5rf3ctgux9azsfnnwqgx4y7x0y5ytsu0nj2z", // staking
   ]);
   const treasury = Number(treasuryRaw) / 1e6;
   return {
