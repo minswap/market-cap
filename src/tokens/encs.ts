4d773373489b4feec686aa6f250816744dd0d04f
encoins <111982001+encryptedcoins@users.noreply.github.com>
2023-07-12 00:24:45 +0200

    Fix circulating

--- src/tokens/encs.ts
+++ src/tokens/encs.ts
@@ -5,10 +5,11 @@ const ENCS = "9abf0afd2f236a19f2842d502d0450cbcd9c79f123a9708f96fd9b96454e4353";
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const blockFrost = getBlockFrostInstance(options);
-  const total = 15_000_000n;
-  const treasury = await getAmountInAddresses(blockFrost, ENCS, [
-  	"stake17ynh8uegtjar9rgdrjfccrer6x674nf58lulned7z5vs6zcrt298r",
+  const total = 15_000_000;
+  const treasuryRaw = await getAmountInAddresses(blockFrost, ENCS, [
+    "stake17ynh8uegtjar9rgdrjfccrer6x674nf58lulned7z5vs6zcrt298r",
   ]);
+  const treasury = Number(treasuryRaw) / 1e6;
   return {
     circulating: (total - treasury).toString(),
     total: total.toString(),
