3e886d2d3f40f0af0f4aa0ac8429a03aa53e2177
nexuscrypt <100082538+nexuscrypt@users.noreply.github.com>
2023-09-18 13:27:00 +0100

    Update chry.ts

--- src/tokens/chry.ts
+++ src/tokens/chry.ts
@@ -4,6 +4,7 @@ import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
 const CHRY = "75fcc276057db5fc48eae0e11453c773c8a54604c3086bf9d95ac1b743485259";
 const TREASURY_ADDRESSES = [
   "stake1uxluurkaazlqyqfqn22acl6lprea52avn6da0q88yk46zfqrc93qj",
+  "stake1u8ffzkegp8h48mare3g3ntf3xmjce3jqptsdtj38ee3yh3c9t4uum", // team tokens held at SEALS' Vending Machine
 ];
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
