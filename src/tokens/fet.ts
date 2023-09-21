2fada483743f65a368c2aa210b8a4c18b2caf9f7
Fraction Estate <140243674+FractionEstate@users.noreply.github.com>
2023-09-21 17:16:16 +0200

    Update fet.ts

--- src/tokens/fet.ts
+++ src/tokens/fet.ts
@@ -15,6 +15,7 @@ const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
     "stake1uxerycuwj09h2n6wydjp4vk936la23p9dvn37ezlkmdl5ycq38hr4", // fe.dev.funds
     "stake1uypwcfxvg7uyzkyeue8fglq6fx7uxmampr2ahylyy4rg38cvky2pr", // fraction.estate
     "stake1ux7pa94sgkzd0yawsu9e9ddj0vr07psyezksfek24m9wn2qtjf62u", // fe.market.funds
+    "stake1uy74mhj5x3jdf8d9446xcwrw723pvqelksxwwxu5c6t799qcnww0q", // fe.charity.fund
     "stake1u8qzt94vyvfvvs4qan02axtfwj2hz4dcqefpehara5rdh4q6h9xka", // founder - daniel johnsen
     "addr1wy08wwdmpp6wtkzzjnghpxn0wtm9zyp69s4fxe675yv06cs70pjxk", // Staking Rewards Vyfi
     "stake1uxmyhzdqv6e57xwkz3z93v996v3ktdctlda44jv76ese5pqwwg7ue", // Farming Rewards Vyfi
