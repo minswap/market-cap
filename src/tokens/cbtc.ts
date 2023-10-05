0783f60e637a9f43a36002d834c862f9450e2006
nexuscrypt <100082538+nexuscrypt@users.noreply.github.com>
2023-10-05 13:46:16 +0000

    totalWithDecimals

--- src/tokens/cbtc.ts
+++ src/tokens/cbtc.ts
@@ -6,10 +6,10 @@ const CBTC = "4190b2941d9be04acc69c39739bd5acc66d60ccab480d8e20bc87e3763425443";
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const blockFrost = getBlockFrostInstance(options);
   const total = await blockFrost.assetsById(CBTC).then((res) => res.quantity);
-  const totalDecimals = Number(total) / 1e8;
+  const totalWithDecimals = Number(total) / 1e8;
   return {
-    circulating: totalDecimals.toString(),
-    total: totalDecimals.toString(),
+    circulating: totalWithDecimals.toString(),
+    total: totalWithDecimals.toString(),
   };
 };
 
