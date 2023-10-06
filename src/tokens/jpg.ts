6848ecb546b27d2e304cd96168b72582da142c41
Joaquin Hoyos (Clark) <clark@jpg.store>
2023-10-06 13:16:46 -0300

    update jpg fetcher (#140)
    feat: update jpg fetcher to remove vesting contract

--- src/tokens/jpg.ts
+++ src/tokens/jpg.ts
@@ -1,11 +1,18 @@
-import { SupplyFetcher } from "../types";
+import { defaultFetcherOptions, SupplyFetcher } from "../types";
+import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
 
-const fetcher: SupplyFetcher = async () => {
+const JPG = "681b5d0383ac3b457e1bcc453223c90ccef26b234328f45fa10fd2764a5047";
+
+const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
+  const blockFrost = getBlockFrostInstance(options);
   const total = 1_000_000_000;
-  const circulating = 200_000_000;
+  const vestingRaw = await getAmountInAddresses(blockFrost, JPG, [
+    "addr1wxhrjawva72parlra9zn8gmv5aeceh3yz666ck7yclfl3ls3vxuqw", // vesting
+  ]);
 
+  const vesting = Number(vestingRaw) / 10 ** 6;
   return {
-    circulating: circulating.toString(),
+    circulating: (total - vesting).toString(),
     total: total.toString(),
   };
 };
