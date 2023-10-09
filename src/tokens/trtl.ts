435af054fe2e75c4a2827ffb1e36ec1a5984585f
Craigslist <108825864+AmPanic@users.noreply.github.com>
2023-10-09 13:54:18 -0400

    Update trtl.ts

--- src/tokens/trtl.ts
+++ src/tokens/trtl.ts
@@ -1,29 +1,23 @@
-import { defaultFetcherOptions, SupplyFetcher } from '../types';
-import { getAmountInAddresses, getBlockFrostInstance } from '../utils';
+import { defaultFetcherOptions, SupplyFetcher } from '../types'
+import { getAmountInAddresses, getBlockFrostInstance } from '../utils'
 
-const TRTL = '52162581184a457fad70470161179c5766f00237d4b67e0f1df1b4e65452544c';
+const trtl = '52162581184a457fad70470161179c5766f00237d4b67e0f1df1b4e65452544c'
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
-  try {
-    const blockFrost = getBlockFrostInstance(options);
+  const blockFrost = getBlockFrostInstance(options)
+  
+  const total = 356_000_000_000
+  const treasuryRaw = await getAmountInAddresses(blockFrost, trtl, [
+    'stake1u858q8m5vxffh7rpff7p9spcqtvjac0xce620zlvqyd8rcgxfw9lj', // $TRTL
+    'stake1u908d0tv3re46g58vnwndvjewd7s6mccs7k2gecs7f0n7cq5g89gp', // Panic
+  ])
 
-    const total = 356_000_000_000;
-    const treasuryRaw = await getAmountInAddresses(blockFrost, TRTL, [
-      'stake1u858q8m5vxffh7rpff7p9spcqtvjac0xce620zlvqyd8rcgxfw9lj', // $TRTL
-      'stake1u908d0tv3re46g58vnwndvjewd7s6mccs7k2gecs7f0n7cq5g89gp', // Panic
-    ]);
+  const treasury = Number(treasuryRaw)
 
-    const treasury = Number(treasuryRaw);
-
-    return {
-      circulating: (total - treasury).toString(),
-      total: total.toString(),
-    };
-  } catch (error) {
-    // Handle errors here
-    console.error('Error fetching TRTL supply:', error);
-    throw error; // Rethrow the error to indicate that the fetch has failed
+  return {
+    circulating: (total - treasury).toString(),
+    total: total.toString(),
   }
-};
+}
 
-export default fetcher;
+export default fetcher
