c618cf320b29d7c7d6ec5f607c3684f8a8e6c649
Craigslist <108825864+AmPanic@users.noreply.github.com>
2023-10-09 23:31:14 -0400

    Update trtl.ts

--- src/tokens/trtl.ts
+++ src/tokens/trtl.ts
@@ -5,13 +5,13 @@ const TRTL = "52162581184a457fad70470161179c5766f00237d4b67e0f1df1b4e65452544c";
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const blockFrost = getBlockFrostInstance(options);
-  const total = 76_715_880_000;
-  const treasuryRaw = await getAmountInAddresses(blockFrost, SNEK, [
+  const total = 36_000_000_000;
+  const treasuryRaw = await getAmountInAddresses(blockFrost, TRTL, [
     "stake1u8vvynj8wlj8ajje5qdrl7g2nla7579ltwz5v7hwr7dt2jc88zglw", // $trtlcoinada
     "addr1q9vxrskmzygzlgmans4rep35m4wutvzu89t202eag9gnnshgwq0hgcvjn0uxzjnuztqrsqke9ms7d3n55797cqg6w8ssecay", // $trtlcoinvault
   ]);
 
-  const burnRaw = await getAmountInAddresses(blockFrost, SNEK, [
+  const burnRaw = await getAmountInAddresses(blockFrost, TRTL, [
     "addr1x8e0d4lvg4xtjyxghvfyc06jcdx9mxps2gjffng2m8q4e68j7mt7c32vhygv3wcjfsl49s6vtkvrq53yjnxs4kwptn5qnmjfck", //$burntrtl
   ]);
 
