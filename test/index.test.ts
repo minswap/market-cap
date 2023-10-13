ef42aa433d81a4f69808e2c7da363735ec1b0b95
Nguyen Le Vu Long <vulongvn98@gmail.com>
2023-10-13 11:01:13 +0700

    test fetchers in batch of 50
    Signed-off-by: Nguyen Le Vu Long <vulongvn98@gmail.com>

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,6 +2,7 @@ import { SupplyFetcher, supplyFetchers } from "../src";
 
 describe("supply fetchers", () => {
   const fetchers: [string, SupplyFetcher][] = [];
+
   if (process.env["ONLY_TEST"]) {
     const token = process.env["ONLY_TEST"];
     const fetcher = supplyFetchers[token];
@@ -11,22 +12,27 @@ describe("supply fetchers", () => {
     fetchers.push(...Object.entries(supplyFetchers));
   }
 
-  test.concurrent.each(fetchers)(
-    `test fetcher for token %s`,
-    async (_, fetcher) => {
-      const resp = await fetcher({ timeout: 20_000 });
-      if (resp.circulating !== undefined) {
-        expect(typeof resp.circulating).toBe("string");
-      }
+  // test 50 fetchers in a batch to avoid rate-limiting
+  for (let i = 0; i < fetchers.length; i += 50) {
+    const fetchersBatch = fetchers.slice(i, i + 50);
 
-      if (resp.total !== undefined) {
-        expect(typeof resp.total).toBe("string");
-      }
+    test.concurrent.each(fetchersBatch)(
+      `test fetcher for token %s`,
+      async (_, fetcher) => {
+        const resp = await fetcher({ timeout: 20_000 });
+        if (resp.circulating !== undefined) {
+          expect(typeof resp.circulating).toBe("string");
+        }
 
-      if (process.env["ONLY_TEST"]) {
-        // eslint-disable-next-line no-console
-        console.debug(resp);
+        if (resp.total !== undefined) {
+          expect(typeof resp.total).toBe("string");
+        }
+
+        if (process.env["ONLY_TEST"]) {
+          // eslint-disable-next-line no-console
+          console.debug(resp);
+        }
       }
-    }
-  );
+    );
+  }
 });
