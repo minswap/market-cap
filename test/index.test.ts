7f458caa33e92531d25cc4d1e501ebfa05775416
Nguyen Le Vu Long <vulongvn98@gmail.com>
2023-10-13 11:18:46 +0700

    test sequentially instead of concurrently
    Signed-off-by: Nguyen Le Vu Long <vulongvn98@gmail.com>

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,7 @@
 import { SupplyFetcher, supplyFetchers } from "../src";
 
+const delay = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms));
+
 describe("supply fetchers", () => {
   const fetchers: [string, SupplyFetcher][] = [];
 
@@ -12,27 +14,21 @@ describe("supply fetchers", () => {
     fetchers.push(...Object.entries(supplyFetchers));
   }
 
-  // test fetchers in batch to avoid rate-limiting
-  for (let i = 0; i < fetchers.length; i += 10) {
-    const fetchersBatch = fetchers.slice(i, i + 10);
-
-    test.concurrent.each(fetchersBatch)(
-      `test fetcher for token %s`,
-      async (_, fetcher) => {
-        const resp = await fetcher({ timeout: 20_000 });
-        if (resp.circulating !== undefined) {
-          expect(typeof resp.circulating).toBe("string");
-        }
-
-        if (resp.total !== undefined) {
-          expect(typeof resp.total).toBe("string");
-        }
-
-        if (process.env["ONLY_TEST"]) {
-          // eslint-disable-next-line no-console
-          console.debug(resp);
-        }
+  for (const [token, fetcher] of fetchers) {
+    test(`test fetcher for token ${token}`, async () => {
+      const resp = await fetcher({ timeout: 20_000 });
+      if (resp.circulating !== undefined) {
+        expect(typeof resp.circulating).toBe("string");
       }
-    );
+      if (resp.total !== undefined) {
+        expect(typeof resp.total).toBe("string");
+      }
+      if (process.env["ONLY_TEST"]) {
+        // eslint-disable-next-line no-console
+        console.debug(resp);
+      }
+      // Delay for 110ms to avoid hitting the rate limit of 10 calls/second
+      await delay(110);
+    });
   }
 });
