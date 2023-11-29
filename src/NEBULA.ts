466337fa58881af3f774bed0535e9deebef01677
ItsNoah21 <134802850+ItsNoah21@users.noreply.github.com>
2023-11-29 17:09:16 -0500

    Add files via upload

--- src/NEBULA.ts
+++ src/NEBULA.ts
@@ -1,23 +1,28 @@
 import { defaultFetcherOptions, SupplyFetcher } from "../types";
 import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
 
-const NEBULA = "3744d5e39333c384505214958c4ed66591a052778512e56caf420f62";
-
-const TEAM_ADDRESSES = [
-  "addr1q8s7yqngv2lpwrf6e3wn0qplz6dsw2f9scdex78uxfg58xv69yv7hnkjauqsj33hktr6ygamt58m7rsjm4elpmlaycssdyaju6", // team
-  "addr1qyruqqptlds9ruwxq45y28669y7q4qu233hmzt2ck260f2s296su9q00c29c8ud846jhyayzeprsa4fygumw0ycrmgssxu8q3z", // dev Vault
-  "addr1qxte5udxtcgd32grn4pd2w2faw7cax3p4lj22e6exmftu28ezkp45z5cs5g5yfpksh8uupjrzkjjyv3c2t23ah04s0tq40n2va", // burn
-];
+const NEBULA = "3744d5e39333c384505214958c4ed66591a052778512e56caf420f624e4542554c41";
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const blockFrost = getBlockFrostInstance(options);
-  const total = 1e9; // 1 billion
-  const treasury = Number(
-    await getAmountInAddresses(blockFrost, NEBULA, TEAM_ADDRESSES),
-  );
-  return {
-    circulating: (total - treasury).toString(),
-    total: total.toString(),
+  const total = 1_000_000_000;
+  const treasuryRaw = await getAmountInAddresses(blockFrost, NEBULA, [
+    "stake1uy9zagwzs8hu9zur7xn6aftjwjpvs3cw65jywdh8jvpa5ggvvlr08 ", //Put the NEBULA treasury addresses here
+    "stake1uy9zagwzs8hu9zur7xn6aftjwjpvs3cw65jywdh8jvpa5ggvvlr08",
+    "stake1uxdzjx0temfw7qgfgcmm93azywa46ralpcfd6ulsal7jvggjrk6ew",
+  ]);
+
+  const burnRaw = await getAmountInAddresses(blockFrost, NEBULA, [
+    "stake1u8u3tq66p2vg2y2zysmgtn7wqep3tffzxgu994g7mh6c84skgl0g7", 
+  ]);
+
+  const treasury = Number(treasuryRaw);
+  const burn = Number(burnRaw);
+
+
+  return{
+    circulating: (total - treasury - burn).toString(),
+    total: (total - burn).toString(),
   };
 };
 
