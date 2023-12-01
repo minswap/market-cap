1211c2874bd19266a91bc1abcd04d68b50e11e59
cobus_coetsee <cobus@matogen.com>
2023-12-01 21:51:58 +0200

    Added MarketRaker circulating supply to MinSwap

---
+++ src/tokens/raker.ts
@@ -0,0 +1,21 @@
+import { defaultFetcherOptions, SupplyFetcher } from "../types";
+import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
+
+const RAKER = "ace2ea0fe142a3687acf86f55bcded860a920864163ee0d3dda8b60252414b4552";
+const TREASURY_ADDRESSES = [
+  "addr1q8djvsvq45pff7vmyvqdujpudkwpzr3cdejqsxg5r6qg60utvn6keknqc0ea3rs3c96naytguxq3pspqn5fkjltmr95srtewf6", // main wallet
+];
+
+const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
+  const blockFrost = getBlockFrostInstance(options);
+  const total = 10_614_254 * 1e6; // 10,614,254 million
+  const treasury =
+    Number(await getAmountInAddresses(blockFrost, RAKER, TREASURY_ADDRESSES)) /
+    1e6;
+  return {
+    circulating: (total - treasury).toString(),
+    total: total.toString(),
+  };
+};
+
+export default fetcher;
