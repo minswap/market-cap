5e3126952ff4d59a3277312ae584e9d6dccaef79
Frank Baris <frank.baris@gmail.com>
2023-12-28 17:05:09 -0700

    Create fire.ts

---
+++ src/tokens/fire.ts
@@ -0,0 +1,23 @@
+import { defaultFetcherOptions, SupplyFetcher } from "../types";
+import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
+
+const FIRE = "a4da8764a57e66a0085b5bfcde96c89b798d92ee83a75f59237e375b46495245";
+
+const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
+  const blockFrost = getBlockFrostInstance(options);
+  const total = 500_000_000;
+  const treasuryRaw = await getAmountInAddresses(blockFrost, FIRE, [
+    "stake1ux7k5ztvhwj7ykv5v7vwjjzq8ckjk0v74z9p9m5w0t55f9clf62eq", // MinSwap Farm Rewards
+    "addr1w8tqqyccvj7402zns2tea78d42etw520fzvf22zmyasjdtsv3e5rz", // DDz Rewards
+    "stake1u94e6kguu8zzpaeevrnagm2f4u4huhh6uazupt3jytznumq93v7dh", // Charity Wallet
+  ]);
+
+  const treasury = Number(treasuryRaw);
+
+  return {
+    circulating: (total - treasury).toString(),
+    total: total.toString(),
+  };
+};
+
+export default fetcher;
