e774e775446bfe264491edb8a884862f4707d102
Philip <philipmarsh152@gmail.com>
2024-01-05 17:36:01 +0100

    Update total supply of pavia to 1B

--- src/tokens/pavia.ts
+++ src/tokens/pavia.ts
@@ -5,7 +5,7 @@ const PAVIA =
   "884892bcdc360bcef87d6b3f806e7f9cd5ac30d999d49970e7a903ae5041564941";
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
-  const total = 2e9;
+  const total = 1e9;
   const blockFrost = getBlockFrostInstance(options);
   const treasury = await getAmountInAddresses(blockFrost, PAVIA, [
     "stake1uxy8jh76wavelv6nc3mjnka0v5uveu45znq3aykx90lam4cdxdumd",
