998300c8da9fa874c25980ac230251f7c8eced7e
Legionaaa <73247112+Legionaaa@users.noreply.github.com>
2024-01-07 09:31:59 +0000

    Create tool.ts

---
+++ src/tokens/tool.ts
@@ -0,0 +1,20 @@
+import { defaultFetcherOptions, SupplyFetcher } from "../types";
+import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
+
+const TOOL = "ac015c38917f306a84748c2d646bed90bdd64421c592163e60702d73";
+
+const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
+  const blockFrost = getBlockFrostInstance(options);
+  const total = 1_000_000_000;
+  const treasuryRaw = await getAmountInAddresses(blockFrost, TOOL, [
+    "addr1vxndsnat5pgddcaqcddu3epun8gvh42yt4z6ekcr8p58wagnl5ek7", // TOOL TREASURY 
+  ]);
+  const treasury = Number(treasuryRaw);
+  
+  return {
+    circulating: (total - treasury).toString(),
+    total: total.toString(),
+  };
+};
+
+export default fetcher;
