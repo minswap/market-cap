fafb18e89aa7abcd16ba178841af6d581d634eda
somethingnfts <120790931+workingdeadnft@users.noreply.github.com>
2024-01-08 13:56:52 +0000

    fix: update work circulating supply

--- src/tokens/work.ts
+++ src/tokens/work.ts
@@ -9,9 +9,12 @@ const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const total = 300_000_000n;
 
   const treasury = await getAmountInAddresses(blockFrost, WORK, [
+    "addr1vy8vhnktsk9vkqjq04ltnj5nk7pezdpqlnt73vnk2d32tjca2awws", // Platform Wallet
+    "stake1u943hr3hqpmk6yc4a2vyk5vkjrngj8kdfaz28r52flmn5hgcaczcj", // $work.courses
+    "stake1u872kmf52n3jdf76rcklllpp6l5w5nmjtzeclvhzysq583qurlgzl", // $work.liquidity
+    "stake1uxrwm3hzkjhulhv6nhgvmhw4lq6ckq9z5xdd8prt27dyg4cx3p6w7", // $work.team
+    "stake1uyt07yq2fdkvmpvgvt24ge4r5luv0h76akmmx7prrs5k94q09rpc7", // $work.treasury
     "stake1u9l0ry8y7a4nl70sjjsg4v76p59sdq43cf7yh9ew9xxp0lg7vyqjw",
-    "addr1v8d9p0fqrwxkp63nspcv47esgrmjhv9hjfv8z6ptyludlnqhwdlhh",
-    "addr1vxauj43mtwwfh0nxfht2g7z6cpt4ksmsaqwrugdeeanv4cgltw6dp",
   ]);
 
   return {
@@ -21,3 +24,4 @@ const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
 };
 
 export default fetcher;
+
