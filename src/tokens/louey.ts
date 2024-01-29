3cfddce6d2dd2517e6efdf8852cc7d291dc88c86
Louey <146684776+LoueyXYZ@users.noreply.github.com>
2024-01-29 15:55:10 +1300

    Louey Circulating Supply (#216)
    Updating the circulating supply of $LOUEY

---
+++ src/tokens/louey.ts
@@ -0,0 +1,22 @@
+import { defaultFetcherOptions, SupplyFetcher } from "../types";
+import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
+
+const LOUEY = "ac11a1a07f04ec9efee4c46c359725922377ec5a4596bbed670cc920";
+const TREASURY_ADDRESSES = [
+  "addr1qy6raw2ndg60u68g0rd079a84qh4y6625gysvzukc6r7u39247zav5n3h2vcpp2sktqxsaa8v44jypar3vk24gjs2jgqsy8hcv", // team
+  "addr1q9esm0mhkhdjk8z83lcur4gwldygyedk8fxmhzldd7lp304kn0xh732vdqjwem8g3vzjhwh9s9fgteu4wnhmncw3j9nsmr4gl5", // marketing
+];
+
+const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
+  const blockFrost = getBlockFrostInstance(options);
+  const total = 100000000; // 100 million
+  const treasury =
+    Number(await getAmountInAddresses(blockFrost, LOUEY, TREASURY_ADDRESSES)) /
+    1e6;
+  return {
+    circulating: (total - treasury).toString(),
+    total: total.toString(),
+  };
+};
+
+export default fetcher;
