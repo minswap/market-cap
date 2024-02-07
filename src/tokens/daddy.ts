b48ddb3ddeeadcf522688b1c76bce895b024144d
gitpool <71734613+poolstakes@users.noreply.github.com>
2024-02-06 23:07:04 -0500

    Update daddy.ts

--- src/tokens/daddy.ts
+++ src/tokens/daddy.ts
@@ -1,16 +1,14 @@
-import { defaultFetcherOptions, SupplyFetcher } from "../types";
-import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
-
-const DADDY = "6e6098846537ca48ff18eac70b7f5040c69ba2c5570521b6eb00fdf5";
-
-const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
-  const blockFrost = getBlockFrostInstance(options);
+import { SupplyFetcher } from "../types";
+ 
+const fetcher: SupplyFetcher = async () => {
   const total = 100_000_000_000;
-  
   return {
-    circulating: total.toString(),
     total: total.toString(),
   };
 };
-
+ 
 export default fetcher;
+
+
+
+
