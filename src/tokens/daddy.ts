ad8c8a3027324f0602dbd33c56bc2bdabc8f22bd
Ben Elferink <ben.elferink@icloud.com>
2024-02-16 18:45:48 +0200

    fix(): run format

--- src/tokens/daddy.ts
+++ src/tokens/daddy.ts
@@ -1,7 +1,8 @@
 import { defaultFetcherOptions, SupplyFetcher } from "../types";
 import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
 
-const DADDY = "6e6098846537ca48ff18eac70b7f5040c69ba2c5570521b6eb00fdf56461646479";
+const DADDY =
+  "6e6098846537ca48ff18eac70b7f5040c69ba2c5570521b6eb00fdf56461646479";
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const blockFrost = getBlockFrostInstance(options);
@@ -10,7 +11,7 @@ const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
 
   const burnRaw = await getAmountInAddresses(blockFrost, DADDY, [
     "addr1w8qmxkacjdffxah0l3qg8hq2pmvs58q8lcy42zy9kda2ylc6dy5r4", // SNEK burn address
-     ]);
+  ]);
 
   const burn = Number(burnRaw);
 
