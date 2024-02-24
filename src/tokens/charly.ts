9b0c5382e086659d6fbb5ad7f2821f785ead8f44
Fred <frog357@gmail.com>
2024-02-24 14:36:46 -0500

    Update charly.ts
    There was a mis-understanding of which wallets the client wanted to be considered.

--- src/tokens/charly.ts
+++ src/tokens/charly.ts
@@ -8,15 +8,6 @@ const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const blockFrost = getBlockFrostInstance(options);
   const total = 777_777_777_777;
   const treasuryRaw = await getAmountInAddresses(blockFrost, CHARLY, [
-    "addr1qyzaa27uuyxfwrqzlz8jmgtm4r7yve5hn9nggt9una63e6eqtwap4qgyz64kp3mwdqukkg3h989cy6js75pd5v6wmynsekglu3", // Adalot_vend
-    "addr1qx9xpwhecsukyjs0ztdf5f6ng5sq5pxepjckeanf3yyac9h4p6zqcdkjfgpt34988p3aje2q64q7kh49492p7rqsu7yqdrcn2g", // Adalot_give
-    "stake1u8ffzkegp8h48mare3g3ntf3xmjce3jqptsdtj38ee3yh3c9t4uum", // Tosidrop
-    "addr1qxkmr0m22xeqludcg5rjdmecjxasu9fat0680qehtcsnftaadgykewa9ufvegeuca9yyq03d9v7ea2y2zthgu7hfgjtsddp6gr", // Minswap_farm
-    "addr1vysfrd9nkmh7u57xg66puszpdw899q4ymf786evge06na3qg4e20s", // Cardano_Lands
-    "addr1wyv79rvp7j8hul6xrh3u5vlcxy0h7gun4tmq3l4e2g3j43sgv5eqq", // Dripdropz
-    "stake1u8zu44gstz27rjwryce03tg3u0mhzk05xu0tzae8dc23t5gspps5d", // ADAYield
-    "addr1v8vx2fyt2vlses0s5n03kckfnsmvc72pwgkq5dgnnkx78ug4cgwak", // Tekmirio
-    "addr1qxq9k3hku6s9mn74cfn485v5p8sxjuwqxs6mvwear4kerfja204vsnpyn436tj4kg2h3vl8flpaszl4a0lhgrq33533qf8qxc6", // Cetf_pool5
     "addr1vxgts9tzm59h72uetsg48m3tj4z6h0gqz3m7pdm0gapwxxslqglt9", // Cardano_Lands2
     "addr1v9gs0trlcmyty7jakcewjs3h00a7xrzyd5wnyfrpeg4wjts0ugx63", // Charly_vend
     "addr1q9mwfll6mrhyapf50mkqhlttxffas3v5urhgsm4l448mhr4vvhmy90lh87378gmwx6jdr5xfgtuucx4dfchlh4nc0peqrxm5kx", // Foundation
