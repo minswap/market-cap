59673205a0e4e7560eefd4aff4e92aa7ee4c9ed9
bitcoincabbage <161375351+bitcoincabbage@users.noreply.github.com>
2024-02-27 12:14:47 +0000

    cbtc total supply

--- src/tokens/cbtc.ts
+++ src/tokens/cbtc.ts
@@ -4,8 +4,8 @@ import { getBlockFrostInstance } from "../utils";
 const CBTC = "4190b2941d9be04acc69c39739bd5acc66d60ccab480d8e20bc87e3763425443";
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
-  const total = 80_000_000;
-  const circulating = 80_000_000;
+  const total = 21_000_000;
+  const circulating = 19_550_000;
   return {
     total: total.toString(),
     circulating: circulating.toString(),
