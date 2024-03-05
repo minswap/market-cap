8ebf8fac3537eacaea35e7b2a1796376a1465708
bitcoincabbage <161375351+bitcoincabbage@users.noreply.github.com>
2024-03-05 15:36:08 +0000

    fixes

--- src/tokens/cbtc.ts
+++ src/tokens/cbtc.ts
@@ -1,9 +1,8 @@
-import { defaultFetcherOptions, SupplyFetcher } from "../types";
-import { getBlockFrostInstance } from "../utils";
+import { /*defaultFetcherOptions,*/ SupplyFetcher } from "../types";
 
-const CBTC = "4190b2941d9be04acc69c39739bd5acc66d60ccab480d8e20bc87e3763425443";
+//const CBTC = "4190b2941d9be04acc69c39739bd5acc66d60ccab480d8e20bc87e3763425443";
 
-const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
+const fetcher: SupplyFetcher = async (/*options = defaultFetcherOptions*/) => {
   const total = 21_000_000;
   const circulating = 19_550_000;
   return {
