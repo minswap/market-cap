0f08a529c63e3253eace7f997253fba1b9776602
PatrickTobler <129864078+PatrickTobler@users.noreply.github.com>
2024-03-11 12:28:27 +0100

    Updated NMKR Token v2 (#276)
    * Update nmkr.ts
    
    * Update nmkr.ts
    
    * Update nmkr.ts
    
    * Update nmkr.ts
    
    * Fix BigInt Bug
    
    Why is this happening?!
    
    * Update nmkr.ts

--- src/tokens/nmkr.ts
+++ src/tokens/nmkr.ts
@@ -6,14 +6,15 @@ const NMKR = "5dac8536653edc12f6f5e1045d8164b9f59998d3bdc300fc928434894e4d4b52";
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
 const blockFrost = getBlockFrostInstance(options);
-const total = 10_000_000_000;
-const treasury = await getAmountInAddresses(blockFrost, NMKR, [
+const total = Number(10_000_000_000);
+const treasuryRaw = await getAmountInAddresses(blockFrost, NMKR, [
   "addr1q9j2atke2qg7ljjm795u2mf4wf4f2uk35f5t7984t62kqtxh7t5nw0qtt7g322gtayqrr7zmpvdrf24kc284uwmhqgas34y7tg",
 ]);
-return {
-  circulating: (BigInt(total) - BigInt(treasury)).toString(),
-  total: total.toString(),
-};
+  const treasury = Number(treasuryRaw) / 1e6;
+  return {
+    circulating: (total - treasury).toString(),
+    total: (total).toString(),
+  };
 };
 
 export default fetcher;
