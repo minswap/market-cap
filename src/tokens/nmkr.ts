7667e3f741f2d1867fe6219390d951644fb2e74e
PatrickTobler <129864078+PatrickTobler@users.noreply.github.com>
2024-03-11 12:05:33 +0100

    Fix BigInt Bug
    Why is this happening?!

--- src/tokens/nmkr.ts
+++ src/tokens/nmkr.ts
@@ -6,7 +6,7 @@ const NMKR = "5dac8536653edc12f6f5e1045d8164b9f59998d3bdc300fc928434894e4d4b52";
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
 const blockFrost = getBlockFrostInstance(options);
-const total = 10_000_000_000;
+const total = Number(10_000_000_000);
 const treasury = await getAmountInAddresses(blockFrost, NMKR, [
   "addr1q9j2atke2qg7ljjm795u2mf4wf4f2uk35f5t7984t62kqtxh7t5nw0qtt7g322gtayqrr7zmpvdrf24kc284uwmhqgas34y7tg",
 ]);
