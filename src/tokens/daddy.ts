a1117b63a1d017417f3e0b58b927393c701de29c
gitpool <71734613+poolstakes@users.noreply.github.com>
2024-04-03 18:55:23 -0400

    Add files via upload

--- src/tokens/daddy.ts
+++ src/tokens/daddy.ts
@@ -1,8 +1,7 @@
 import { defaultFetcherOptions, SupplyFetcher } from "../types";
 import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
 
-const DADDY =
-  "6e6098846537ca48ff18eac70b7f5040c69ba2c5570521b6eb00fdf56461646479";
+const DADDY = "6e6098846537ca48ff18eac70b7f5040c69ba2c5570521b6eb00fdf56461646479";
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const blockFrost = getBlockFrostInstance(options);
@@ -11,14 +10,13 @@ const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
 
   const burnRaw = await getAmountInAddresses(blockFrost, DADDY, [
     "addr1w8qmxkacjdffxah0l3qg8hq2pmvs58q8lcy42zy9kda2ylc6dy5r4", // SNEK burn address
-  ]);
+     ]);
 
-  let burn = Number(burnRaw);
-  if (Number.isNaN(burn)) burn = 0;
+  const burn = Number(burnRaw);
 
   return {
     circulating: (total - burn).toString(),
-    total: (total - burn).toString(),
+    total: (total).toString(),
   };
 };
 
