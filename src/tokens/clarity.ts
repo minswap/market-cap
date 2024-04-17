eb10c0f2d2a9614dd04132399670ee1c35ffb9ea
jacobwarren44 <jacob@taptools.io>
2024-04-17 11:32:10 -0600

    add dao

--- src/tokens/clarity.ts
+++ src/tokens/clarity.ts
@@ -11,6 +11,7 @@ const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
     "stake1u934vrjj6cdw5rrd2vewahxu3nrd9wslhjlkvamd5kc8ytce7sjsk", // Strategic Partner Vesting Wallet
     "stake1u8g43cjercws2x26xmtlukgg3nfy4m0l6uw6dqtcp0y5fhc5pm7q5", // Future Development Wallet
     "stake1uxs3nf3qj4xuyv9t63vyhxq8glec3zcxw0a03vegwdzu6hglwwmld", // Team Vesting Wallet
+    "addr1w87vg0x4k2x95dlp7mrlef909lgk25kgajhk2dxzxk0tquclfp0at", // DAO treasury
   ]);
 
   const treasury = Number(treasuryRaw) / 1e6;
