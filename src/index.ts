22b04ab57f35b1e86ac18274b5118d0379a18a04
adakondacoin <135918761+adakondacoin@users.noreply.github.com>
2024-04-24 00:32:59 +0200

    Add $Fren token info (#208)
    * Create fren.ts
    
    * Update fren.ts
    
    * Update fren.ts
    
    * Update fren.ts
    
    * Update index.ts
    
    * Update fren.ts
    
    * Update index.ts
    
    * Update index.ts
    
    * Update index.ts
    
    * Fix prettier and eslint formatting
    
    ---------
    
    Co-authored-by: Patrik <51710571+shadowkora@users.noreply.github.com>
    Co-authored-by: patrik <patrik.soukal106@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -38,6 +38,7 @@ import fetFetcher from "./tokens/fet";
 import fireFetcher from "./tokens/fire";
 import flacFetcher from "./tokens/flac";
 import fldtFetcher from "./tokens/fldt";
+import frenFetcher from "./tokens/fren";
 import gensFetcher from "./tokens/gens";
 import gensxFetcher from "./tokens/gensx";
 import geroFetcher from "./tokens/gero";
@@ -266,6 +267,7 @@ export const supplyFetchers: Record<string, SupplyFetcher> = {
   "961f2cac0bb1967d74691af179350c1e1062c7298d1f7be1e4696e312444455250":
     derpFetcher,
   "2d587111358801114f04df83dc0015de0a740b462b75cce5170fc935434749": cgiFetcher,
+  fc11a9ef431f81b837736be5f53e4da29b9469c983d07f321262ce614652454e: frenFetcher,
   "20cd68533b47565f3c61efb39c30fdace9963bfa4c0060b613448e3c50524f584945":
     proxiesFetcher,
   f6ac48c64aa7af16434d9f84e014d11fba38525b436acc338ff20b0d4d7463: mtcFetcher,
