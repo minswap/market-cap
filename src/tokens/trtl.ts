78d48f39bc130b1b71616fd32816cba04508e1ff
Ben Elferink <ben.elferink@icloud.com>
2024-04-24 02:16:49 +0300

    Updated TRTL burn addresses (#283)
    * add  token
    
    * Add $TRTL burn addresses
    
    * updated TRTL burn addresses

--- src/tokens/trtl.ts
+++ src/tokens/trtl.ts
@@ -16,8 +16,11 @@ const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const treasury = Number(treasuryRaw);
 
   const burnRaw = await getAmountInAddresses(blockFrost, TRTL, [
-    "addr1xy9phln4e5q23w533z7nme7sxr3p6lpldqhsyc8u5qd9c2mudsgax8xq9e4qr8f4yu4cxsghtapg282z5wn0nyjgtmsspwevz8",
+    "addr1xy9phln4e5q23w533z7nme7sxr3p6lpldqhsyc8u5qd9c2mudsgax8xq9e4qr8f4yu4cxsghtapg282z5wn0nyjgtmsspwevz8", // 150 year lock
     "addr1x8gm0yhvdzv2a4n6rm02vfyez6x49nsj2dls4ml7t3rl7tw3k7fwc6yc4mt858k75cjfj95d2t8py5mlpthluhz8luks03cwcj",
+    "addr1z8snz7c4974vzdpxu65ruphl3zjdvtxw8strf2c2tmqnxzwccf8ywaly0m99ngq68lus48lmafut7ku9geawu8u6k49suv42qq", // min contract
+    "addr1w93rz3ae8p3elzn0sqfu37hyzw0up98r2kgjjdmdq9wshmck40yr8", // hosky burn
+    "addr1w8qmxkacjdffxah0l3qg8hq2pmvs58q8lcy42zy9kda2ylc6dy5r4", // snek burn
   ]);
 
   const burn = Number(burnRaw);
