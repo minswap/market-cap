fdb70a3c170544669df4d072205d9e74db3a9b4c
Sebastian Thobe <sebastianthobe@googlemail.com>
2024-04-25 01:46:18 +0200

    Adds $SPX  market cap fetcher (#296)
    adds  market cap fetcher
    
    Co-authored-by: Patrik <51710571+shadowkora@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -103,6 +103,7 @@ import snepeFetcher from "./tokens/snepe";
 import snowFetcher from "./tokens/snow";
 import societyFetcher from "./tokens/society";
 import spfFetcher from "./tokens/spf";
+import spxFetcher from "./tokens/spx";
 import stableFetcher from "./tokens/stable";
 import sundaeFetcher from "./tokens/sundae";
 import tedyFetcher from "./tokens/tedy";
@@ -338,6 +339,7 @@ export const supplyFetchers: Record<string, SupplyFetcher> = {
     adyFetcher,
   "8c904e09d2ba206d78e6904133659dadf827e478a8f71beb8e4a823a504f524b45":
     porkeFetcher,
+  f7516c9f7b347eb412a777f3c711099b199ccd2be23b568a4a3abf6d535058: spxFetcher,
   "5b26e685cc5c9ad630bde3e3cd48c694436671f3d25df53777ca60ef4e564c": nvlFetcher,
   "1e76aaec4869308ef5b61e81ebf229f2e70f75a50223defa087f807b436c61726974792044414f20546f6b656e":
     clarityFetcher,
