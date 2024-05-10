0f54552951d9773704a057043fafd1eae3e4bb7f
Intraconnection <162527459+Intraconnection@users.noreply.github.com>
2024-05-10 13:26:23 -0400

    Update smoke.ts

--- src/tokens/smoke.ts
+++ src/tokens/smoke.ts
@@ -5,7 +5,7 @@ const SMOKE = "dd00d877798443a2de57e5bc667784168616aa8244b558743e448784534d4f4b4
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const blockFrost = getBlockFrostInstance(options);
-  const total = 76_715_880_000;
+  const total = 6_900_000_000;
   const treasuryRaw = await getAmountInAddresses(blockFrost, SMOKE, [
     "stake1uyzckf6tcr4d3mqu9y9z8xuqmjygsnj0pr5440fls49su8c4xygss", // $smoketeam
     "stake1uxqs97h0whyzrfjesqn93gzv6k2e6chgw74ze3e20djtshgcq8xk4", // $smokemarketing
