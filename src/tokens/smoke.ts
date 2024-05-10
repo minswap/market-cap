d9b1969097b7d1e86f150bdf194b10bdad8de239
Intraconnection <162527459+Intraconnection@users.noreply.github.com>
2024-05-10 13:25:41 -0400

    Update smoke.ts

--- src/tokens/smoke.ts
+++ src/tokens/smoke.ts
@@ -1,25 +1,21 @@
 import { defaultFetcherOptions, SupplyFetcher } from "../types";
 import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
 
-const SNEK = "279c909f348e533da5808898f87f9a14bb2c3dfbbacccd631d927a3f534e454b";
+const SMOKE = "dd00d877798443a2de57e5bc667784168616aa8244b558743e448784534d4f4b45";
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const blockFrost = getBlockFrostInstance(options);
   const total = 76_715_880_000;
-  const treasuryRaw = await getAmountInAddresses(blockFrost, SNEK, [
-    "stake1u8zjwv6a8ztrl9xkcc90utn32y4jsrxep90z2967c5aqv6cxchhhv", // $snekcoinada
-    "stake1u8ncs7903m7pxzfyrzxagzx0aey4aus2533eeqlnevw0h4qs2f82r", // $snekcoinvault
-  ]);
-
-  const burnRaw = await getAmountInAddresses(blockFrost, SNEK, [
-    "addr1w8qmxkacjdffxah0l3qg8hq2pmvs58q8lcy42zy9kda2ylc6dy5r4", //$burnsnek
+  const treasuryRaw = await getAmountInAddresses(blockFrost, SMOKE, [
+    "stake1uyzckf6tcr4d3mqu9y9z8xuqmjygsnj0pr5440fls49su8c4xygss", // $smoketeam
+    "stake1uxqs97h0whyzrfjesqn93gzv6k2e6chgw74ze3e20djtshgcq8xk4", // $smokemarketing
   ]);
 
   const treasury = Number(treasuryRaw);
-  const burn = Number(burnRaw);
+  
   return {
-    circulating: (total - treasury - burn).toString(),
-    total: (total - burn).toString(),
+    circulating: (total - treasury).toString(),
+    total: (total).toString(),
   };
 };
 
