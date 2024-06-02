932ae46d3bb5df5780bef34c7b90781b7fe20c8f
xEVILWARx <28636031+xEVILWARx@users.noreply.github.com>
2024-06-02 17:33:34 +0330

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -359,5 +359,5 @@ export const supplyFetchers: Record<string, SupplyFetcher> = {
     clarityFetcher,
   "6947eccc74ebf8c1716339b97af768bfbc70d330a743b79bbc5ccdeb544554": tetFetcher,
   "cdbbe391853676285131fe2de250e274dbeb5f9d98344e86c7b383d9474c44534e454b": 
-    gldsnekFetcher,
+    gldsnekFetcher, //test
 };
