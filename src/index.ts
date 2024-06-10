ef6191a224785b688f4cadaf479c3220aa85910c
jacobwarren44 <jacob@taptools.io>
2024-06-10 09:43:42 -0400

    GOKEY V2

--- src/index.ts
+++ src/index.ts
@@ -48,6 +48,7 @@ import gensxFetcher from "./tokens/gensx";
 import geroFetcher from "./tokens/gero";
 import gldsnekFetcher from "./tokens/gldsnek";
 import gokeyFetcher from "./tokens/gokey";
+import gokeyv2Fetcher from "./tokens/gokeyv2";
 import herbFetcher from "./tokens/herb";
 import hoskyFetcher from "./tokens/hosky";
 import huntFetcher from "./tokens/hunt";
@@ -359,4 +360,5 @@ export const supplyFetchers: Record<string, SupplyFetcher> = {
   "1e76aaec4869308ef5b61e81ebf229f2e70f75a50223defa087f807b436c61726974792044414f20546f6b656e":
     clarityFetcher,
   "6947eccc74ebf8c1716339b97af768bfbc70d330a743b79bbc5ccdeb544554": tetFetcher,
+  "ab0dec21aa7d939ad124ffaf95faf8ed72b1018ff72fffd8f9974907474f4b4559": gokeyv2Fetcher,
 };
