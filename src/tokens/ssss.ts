0b9b6e97038a5554a1616d068b86e615bf4572d7
NFTKri <166889251+NFTKri@users.noreply.github.com>
2024-06-16 01:51:02 -0400

    Update ssss.ts

--- src/tokens/ssss.ts
+++ src/tokens/ssss.ts
@@ -11,7 +11,7 @@ const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
     "stake17xjrxzlfv3l9dtlykenxxf4zckh287hmwvzrg3pzhq7n6xqg39z66", // $Sssstreasury
   ]);
 
-  const burnRaw = await getAmountInAddresses(blockFrost, gldsnek, [
+  const burnRaw = await getAmountInAddresses(blockFrost, Ssss, [
     "addr1w8qmxkacjdffxah0l3qg8hq2pmvs58q8lcy42zy9kda2ylc6dy5r4", // burn address
   ]);
 
