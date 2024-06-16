934659be8155e39a104f197dac70459b2b268f09
nexuscrypt <100082538+nexuscrypt@users.noreply.github.com>
2024-06-16 14:42:55 +0100

    Update chry

--- src/tokens/chry.ts
+++ src/tokens/chry.ts
@@ -4,7 +4,7 @@ import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
 const CHRY = "75fcc276057db5fc48eae0e11453c773c8a54604c3086bf9d95ac1b743485259";
 const TREASURY_ADDRESSES = [
   "stake17ya8xvcc5h2mkeladwnw8vutr5qya3vau6z9dw7zgs6mmrgtqgz58",
-  "stake1u8ffzkegp8h48mare3g3ntf3xmjce3jqptsdtj38ee3yh3c9t4uum", // Comminuty incentives + team tokens held at TosiDrop
+  "stake1u8ffzkegp8h48mare3g3ntf3xmjce3jqptsdtj38ee3yh3c9t4uum", // Community incentives + team tokens held at TosiDrop
 ];
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
