5be08a3933b53ad14472efceac02b052546aff24
similiken <122702393+similiken@users.noreply.github.com>
2024-06-17 21:14:27 -0400

    ashib and mtc (#328)
    Removes circulation supply; that's sort of a pain to keep up with.

--- src/tokens/ashib.ts
+++ src/tokens/ashib.ts
@@ -7,27 +7,13 @@ const ASHIB =
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const blockFrost = getBlockFrostInstance(options);
   const total = 1_000_000_000_000;
-  const treasuryRaw = await getAmountInAddresses(blockFrost, ASHIB, [
-    "addr1qxpv50tpyy9xutrzd3585eup87wm0qv3zyw4jfawqzchgyfltcw74v5edmr2veq0dvu4262jhendzee24ellgcamlqzsfzzfsq", // treasury 1
-    "addr1q8dhkxncaznxd66tq5npvuqxl87zdw0glpazysxckl9gxfecnrdy6frmvrk2szfx2wsg9wa3z3wzctfmgaw7hcts7g6quzkakx", // treasury 2
-    "addr1q8my0y2n7ythvevuyfur4w4akrc6mp454p2yp4pkjcrlwq2zhw8y7c92vew3fqwy7u4yrq66d2yt8wmd0tm3uyhsxaps59yy9y", // treasury 3
-    "addr1qxrlzwkmv826sps9sfjdg3cr8malcr2wmfvhvt0sqpd84xaq4rezcdmnr9f52x0zsgpz3zl8klrwjhmynksx5lhs5scsdqcx9d", // treasury 4
-    "addr1qxkmr0m22xeqludcg5rjdmecjxasu9fat0680qehtcsnftaadgykewa9ufvegeuca9yyq03d9v7ea2y2zthgu7hfgjtsddp6gr", // minswap farm distribution
-    "addr1qxpeczj2lluh88sa2g3tm0e3fj39c34rlctmvcszjs67g6wjj9djsz0020h68nz3rxknzdh93nryqzhq6h9z0nnzf0rshrectt", // tosidrop distribution
-    "addr1w9rerwzk0f5v4den9u2c7anv2d4dl88hq9cq0xgcmernsfsak7w6r", // dripdropz distribution
-    "addr1qx3wvec7uhweerzqaeezzepmrwft8sfpucgk50tsuluz8f8cfhlykwhxy66aazkjmfp4euf0yhpeezx3exncm7e69pvs7jetw6", // starcada distribution
-    "addr1q8xhjg9tn29a6vpv9e52xf8zn7hl7nqkv99kk4uac08myjud244wsz2v65xv0jc4tgjyfxrhgnmcav0upfcvcdv2d8qq7fqgh3", // adalot jackpot vending
-    "addr1qyjhg2ge6w6tzwc9gwmddp5ha07zsawpd3pse7v9a0893jufgx9ne2hurkmz7adclf0tpehwdqwuhfejtph98vpfhulscdnlzz", // adalot exchange vending
-  ]);
 
   const burnRaw = await getAmountInAddresses(blockFrost, ASHIB, [
     "addr1w8qvvu0m5jpkgxn3hwfd829hc5kfp0cuq83tsvgk44752dsea0svn", // burn wallet
   ]);
 
-  const treasury = Number(treasuryRaw) / 1e6;
   const burn = Number(burnRaw) / 1e6;
   return {
-    circulating: (total - treasury - burn).toString(),
     total: (total - burn).toString(),
   };
 };
--- src/tokens/mtc.ts
+++ src/tokens/mtc.ts
@@ -1,25 +1,8 @@
-import { defaultFetcherOptions, SupplyFetcher } from "../types";
-import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
+import { SupplyFetcher } from "../types";
 
-const MTC = "f6ac48c64aa7af16434d9f84e014d11fba38525b436acc338ff20b0d4d7463";
-const TEAM_ADDRESSES = [
-  "addr1qxw6ltkkfapk934cfhj8aswpv6pj9ejlqtuszcr3vp3nkcyl4z8jmxwk2u8knrqgptuc3f56hqxenmp45qscmeyxg0xs96x9qy", // team 1
-  "addr1qy567alv55pznmx8newdj77tghetevq2jr39t5uz7vrk208l95lxy69yvjzpx79wu6lsrq54as3ynftrg0t0ecyfd59sl5xcxv", // team 2
-  "addr1qx5705wkfz897qul8cck2w2ke4uxshal8ghu0m3tncy0sasq9sp8s22znmvqlmka7g20jckxyazqhjehg5q6h3ek0shs9cslz8", // team 3
-  "addr1qx42lkkh8qhnetve4cmx74mzqx4qkssxz9ltmsn8nmgcxxs6gw4tkslgkhp8m2pgkedxlhtx7e8k5dj5xn7yt88p54kq69nh0h", // team 4
-  "addr1q8yll2vw2h2dhv7fccdxwfm95puje3zelredzmw97w34hdwm9dndrhqfvas9nvg7rgn8wrwnzxsj7wp2msuthuvfuwsq9e905k", // snek staked
-  "addr1wxqg5vgp3fsh6ddmr0vhc5xuhrckhyav9emdtwpfmafaf5q63x6rc", //dripdropz bucket
-  "addr1qx3wvec7uhweerzqaeezzepmrwft8sfpucgk50tsuluz8f8cfhlykwhxy66aazkjmfp4euf0yhpeezx3exncm7e69pvs7jetw6", //starcada distribution
-];
-
-const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
-  const blockFrost = getBlockFrostInstance(options);
-  const total = 1e6; // 1 million
-  const treasury = Number(
-    await getAmountInAddresses(blockFrost, MTC, TEAM_ADDRESSES)
-  );
+const fetcher: SupplyFetcher = async () => {
+  const total = 1_000_000;
   return {
-    circulating: (total - treasury).toString(),
     total: total.toString(),
   };
 };
