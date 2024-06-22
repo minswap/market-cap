f8496049efdc3d67be532cc3a9d6d6eb72d4fdc0
PatrickTobler <129864078+PatrickTobler@users.noreply.github.com>
2024-06-22 16:39:16 +0200

    Update sharl.ts

--- src/tokens/sharl.ts
+++ src/tokens/sharl.ts
@@ -1,10 +1,7 @@
-import { defaultFetcherOptions, SupplyFetcher } from "../types";
-import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
+import { SupplyFetcher } from "../types";
 
-const SHARL = "590f6d119b214cdcf7ef7751f8b7f1de615ff8f6de097a5ce62b257b";
-
-const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
-  const total = Number(999_000_000_000);
+const fetcher: SupplyFetcher = async () => {
+  const total = 999_000_000_000;
   return {
     circulating: total.toString(),
     total: total.toString(),
