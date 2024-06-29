ab5a44f01e2d9438f990eea4e5e15cdb49af7f02
Nilay Saha <saha.nilay@gmail.com>
2024-06-29 10:39:11 +0200

    script for circulating supply

---
+++ src/tokens/reit.ts
@@ -0,0 +1,28 @@
+import { defaultFetcherOptions, SupplyFetcher } from "../types";
+import axios  from "axios";
+
+
+const REIT = "52d4b39c2407ce020ab4abb785d820a3ad5a2fa07600d07a205e509f";
+const REIT_ASSET = `${REIT}52454954`
+
+
+const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
+    const total = 50_000_000;
+
+    //get the currently minted supply                                                                                                                                                                                                                                                                 
+    const instance  = axios.create({
+        baseURL: `https://cardano-mainnet.blockfrost.io/api/v0/`,
+        timeout: 1000,
+        headers: { 'project_id': process.env["BLOCKFROST_PROJECT_ID"]}
+    })
+    
+    const assetInfo = await instance.get(`assets/${REIT_ASSET}`)    
+    const total_mint = assetInfo.data.quantity
+
+    return {
+        circulating: total_mint,
+        total: total.toString(),
+    };
+};
+
+export default fetcher;
