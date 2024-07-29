07ea2f5bbcc975f6ae982d1cc7d3f85665e27f6e
adakondacoin <135918761+adakondacoin@users.noreply.github.com>
2024-07-29 21:53:42 +0200

    Update squeak.ts

--- src/tokens/squeak.ts
+++ src/tokens/squeak.ts
@@ -1,14 +1,14 @@
 import { defaultFetcherOptions, SupplyFetcher } from "../types";
 import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
 
-const Squeak = "097f37ef3f64a7967c645cb2a40b67594b0f6f4d187d654ff927403753717565616b";
+const SQUEAK = "097f37ef3f64a7967c645cb2a40b67594b0f6f4d187d654ff927403753717565616b";
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const blockFrost = getBlockFrostInstance(options);
   const total = 1_000_000_000;
-
-  const burnRaw = await getAmountInAddresses(blockFrost, Squeak, [
-    "addr1z8kadskrrswl3avwe9psd3mar5l7shf06slyxr2tetwln4nzg06w6dvxg0vuzx4s0uenlr0vfucctcfwpa2eshd86taqduntvj", //$chasm
+  
+  const burnRaw = await getAmountInAddresses(blockFrost, SQUEAK, [
+    "addr1z8kadskrrswl3avwe9psd3mar5l7shf06slyxr2tetwln4nzg06w6dvxg0vuzx4s0uenlr0vfucctcfwpa2eshd86taqduntvj",
   ]);
 
   const burn = Number(burnRaw);
