dfe74a0e36c1dbf7e94f11a3edb240c642f93ff3
nexuscrypt <100082538+nexuscrypt@users.noreply.github.com>
2024-07-31 10:35:23 +0100

    Update and rename chry.ts to nova.ts

--- src/tokens/nova.ts
+++ src/tokens/nova.ts
@@ -1,7 +1,7 @@
 import { defaultFetcherOptions, SupplyFetcher } from "../types";
 import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
 
-const CHRY = "75fcc276057db5fc48eae0e11453c773c8a54604c3086bf9d95ac1b743485259";
+const NOVA = "75fcc276057db5fc48eae0e11453c773c8a54604c3086bf9d95ac1b743485259";
 const TREASURY_ADDRESSES = [
   "stake17ya8xvcc5h2mkeladwnw8vutr5qya3vau6z9dw7zgs6mmrgtqgz58",
   "stake1u8ffzkegp8h48mare3g3ntf3xmjce3jqptsdtj38ee3yh3c9t4uum", // Community incentives + team tokens held at TosiDrop
@@ -11,7 +11,7 @@ const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const blockFrost = getBlockFrostInstance(options);
   const total = 88e6; // 88 million
   const treasury = Number(
-    await getAmountInAddresses(blockFrost, CHRY, TREASURY_ADDRESSES)
+    await getAmountInAddresses(blockFrost, NOVA, TREASURY_ADDRESSES)
   );
 
   return {
