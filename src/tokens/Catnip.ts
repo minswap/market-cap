096bd0916c21713f13c018ebfd1f8257eec19786
Savvy <141248514+savvycodings@users.noreply.github.com>
2024-08-06 02:15:24 +0200

    Update Catnip.ts

--- src/tokens/Catnip.ts
+++ src/tokens/Catnip.ts
@@ -1,14 +1,14 @@
 import { defaultFetcherOptions, SupplyFetcher } from "../types";
 import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
 
-const DEGA = "633f2e2c5280417c6b76055eda54fc07de984c122c01573ea4a9e823";
+const Catnip = "633f2e2c5280417c6b76055eda54fc07de984c122c01573ea4a9e823";
 const TREASURY_ADDRESSES = ["addr1x8lgmc2vgwkvr9dll5keap2fvga7f6sanu0rt0902skr3mh73hs5csavcx2mllfdn6z5jc3mun4pm8c7xk7274pv8rhqn4669u"];
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const blockFrost = getBlockFrostInstance(options);
   const total = 900000000;
   const treasury = Number(
-    await getAmountInAddresses(blockFrost, DEGA, TREASURY_ADDRESSES)
+    await getAmountInAddresses(blockFrost, Catnip, TREASURY_ADDRESSES)
   );
 
   return {
