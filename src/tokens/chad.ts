a9bd3c480cf5e4294e9f1a86b53097d8e8c63cd1
Kubys <jakub.koucky98@gmail.com>
2024-12-09 16:11:37 +0100

    Add $Chad token

---
+++ src/tokens/chad.ts
@@ -0,0 +1,25 @@
+import { defaultFetcherOptions, SupplyFetcher } from "../types";
+import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
+
+const CHAD = "97075bf380e65f3c63fb733267adbb7d42eec574428a754d2abca55b";
+
+const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
+  const blockFrost = getBlockFrostInstance(options);
+  const total = 1_000_000_000n;
+  const treasuryRaw = await getAmountInAddresses(blockFrost, CHAD, [
+    "stake1uy0sgxfm8pzrkru97rg8pteu4j05c6dgwuwzemhg6k8te3g6h5due", // treasury
+  ]);
+
+  const burnRaw = await getAmountInAddresses(blockFrost, CHAD, [
+    "addr1w8qmxkacjdffxah0l3qg8hq2pmvs58q8lcy42zy9kda2ylc6dy5r4", // burn wallet
+  ]);
+
+  const treasury = Number(treasuryRaw);
+  const burn = Number(burnRaw);
+  return {
+    circulating: (total - treasury - burn).toString(),
+    total: (total - burn).toString(),
+  };
+};
+
+export default fetcher;
