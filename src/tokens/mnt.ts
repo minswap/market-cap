48f2977175fa0ff577b5181ef5eb2abce8feea7e
Nguyễn Thái Bảo <36961697+0xj4m35@users.noreply.github.com>
2024-12-10 10:02:45 +0700

    add CHIPS market-cap (#394)
    * add CHIPS market-cap
    
    Signed-off-by: James Ng <theuranus.ntb@gmail.com>
    
    * update mnt
    
    Signed-off-by: James Ng <theuranus.ntb@gmail.com>
    
    ---------
    
    Signed-off-by: James Ng <theuranus.ntb@gmail.com>

--- src/tokens/mnt.ts
+++ src/tokens/mnt.ts
@@ -1,21 +1,18 @@
 import { defaultFetcherOptions, SupplyFetcher } from "../types";
 import { getAxiosInstance } from "../utils";
 
-// eslint-disable-next-line unused-imports/no-unused-vars
-const MNT = "43b07d4037f0d75ee10f9863097463fc02ff3c0b8b705ae61d9c75bf";
-
-const TOTAL_SUPPLY = 100_000_000;
-
-const MNT_SUPPLY_ADDRESS = "https://www.mynth.ai/api/token-supply";
-
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const axios = getAxiosInstance(options);
-  const response = await axios.get(MNT_SUPPLY_ADDRESS);
-  const treasury = response.data.current_supply;
+  const [totalSupplyResponse, circulatingResponse] = await Promise.all([
+    axios.get("https://www.mynth.ai/api/total-supply"),
+    axios.get("https://www.mynth.ai/api/current-supply"),
+  ]);
+  const totalSupply = Number(totalSupplyResponse.data) / 1e6;
+  const circulating = Number(circulatingResponse.data) / 1e6;
 
   return {
-    circulating: treasury.toString(),
-    total: TOTAL_SUPPLY.toString(),
+    total: totalSupply.toString(),
+    circulating: circulating.toString(),
   };
 };
 
